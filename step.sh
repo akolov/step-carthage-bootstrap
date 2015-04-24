78a33af8bb98136a194711c7b2f2cead15bac490
Alexander Kolov <me@alexkolov.com>
2015-04-24 14:03:34 +0200

    Find Cartfiles and run bootstrap for them

--- step.sh
+++ step.sh
@@ -7,8 +7,31 @@ if ! hash carthage 2>/dev/null; then
   brew install carthage
 fi
 
-echo "=== Will run Carthage boostrap for platform $CARTHAGE_PLATFORM in $THIS_SCRIPT_DIR"
-carthage bootstrap --platform "$CARTHAGE_PLATFORM"
+echo "=== Will run Carthage boostrap for platform $CARTHAGE_PLATFORM"
+
+for cartfile in $(find . -type f -iname 'Cartfile')
+do
+  cartcount=$[cartcount + 1]
+  echo " (i) Cartfile found at: ${cartfile}"
+  curr_cartfile_dir=$(dirname "${cartfile}")
+  curr_cartfile_basename=$(basename "${cartfile}")
+  echo " (i) Cartfile directory: ${curr_cartfile_dir}"
+
+  (
+    echo
+    echo " ===> Carthage bootstrap: ${cartfile}"
+    cd "${curr_cartfile_dir}"
+    carthage bootstrap --platform "$CARTHAGE_PLATFORM" --verbose
+  )
+
+  if [ $? -ne 0 ] ; then
+    echo "!!! Could not bootstrap cartfile: ${cartfile}"
+    exit 1
+  fi
+  echo "* Bootstrapped cartfile: ${cartfile}"
+done
+unset IFS
+
 echo "=== Finished running Carthage boostrap"
 
 exit $?
