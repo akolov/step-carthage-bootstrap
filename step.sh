15c22561807a145a7a3927f096d464a7086c6f41
Alexander Kolov <me@alexkolov.com>
2015-04-24 13:41:14 +0200

    Add logging

--- step.sh
+++ step.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 if ! hash carthage 2>/dev/null; then
+  echo "=== Will install Carthage"
   brew install carthage
 fi
 
+echo "=== Will run Carthage boostrap for platform $CARTHAGE_PLATFORM in $THIS_SCRIPT_DIR"
 carthage bootstrap --platform "$CARTHAGE_PLATFORM"
+echo "=== Finished running Carthage boostrap"
 
 exit $?
